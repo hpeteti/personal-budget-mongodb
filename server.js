abfd631e799f934833a2f18b0e18555ba4aa5b9d
Harsha Peteti <petetiharsha2002@gmail.com>
2023-09-17 19:07:23 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,31 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget:[
-    {
-        title: 'Eat out',
-        budget: 30
-    },
-    {
-        title: 'Rent',
-        budget: 350
-    },
-    {
-        title: 'Groceries',
-        budget: 90
-    },
-]
-};
+const budget = [];
+
+fs.readFile('budget.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        budgetData = JSON.parse(data);
+    }
+});
+
 app.get('/hello', (req,res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req,res) => {
-    res.json(budget);
+    res.json(budgetData);
 });
 
 app.listen(port, () => {
